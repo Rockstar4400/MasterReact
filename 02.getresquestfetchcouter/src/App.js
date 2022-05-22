2b72c08f75a22dee0b0f7de7a781a054bc9c7b94
Alberto Del Angel <rockstar4400@gmail.com>
2022-05-22 14:07:22 -0500

    Updating fetch with state

--- 02.getresquestfetchcouter/src/App.js
+++ 02.getresquestfetchcouter/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FetchGetCounter from './components/FetchGetCounter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+    state = {
+        qoutes: null
+    }
+
+componentDidMount(){
+
+    fetch(
+          "https://gist.githubusercontent.com/Rockstar4400/" +
+          "7678c57b0cebc5f740973ecb7a2691d0/raw/2daef0c7cdd39460787b086bb5af6166ebd56d2f/" +
+          "quotes.json"
+      )
+        .then((response) => response.json())
+        .then((data) =>
+          this.setState({
+            quotes: data.quotes,
+          })
+        );
+
+        }
+
+  render() {
+
+     const quotes = this.state.quotes;
+     console.log(quotes)
+      
+    // const listItems = quotes.map((quote) => (
+    //     <figure
+    //       key={quote.quote}
+    //       author={quote.author}
+    //       house={quote.house}
+    //       className="card-body"
+    //     >
+    //       <blockquote>{quote.quote}</blockquote>
+    //     </figure>
+    //   ));
+    //   const max = this.props.quotes.length - 1;
+    //   const random = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    // if (this.state.quotes == null) {
+         return <FetchGetCounter />;
+    //   } else {
+    //     return <FetchGetCounter id="text" message={listItems[random].key} />;
+    //   }
+     }
 }
 
 export default App;
---
+++ 02.getresquestfetchcouter/src/components/FetchGetCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FetchGetCounter = (props) => {
+  return (
+      
+      <div id={props.id}>
+          {props.message}asd
+      </div>
+      );
+}
+
+export default FetchGetCounter;
